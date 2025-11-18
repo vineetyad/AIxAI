293759279dde74783509a2f6f813166bc5352195
Vineet Yadav <144416932+vineetyad@users.noreply.github.com>
2025-11-19 04:33:12 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Background } from './components/Background';
+import { Header } from './components/Header';
+import { Hero } from './components/Hero';
+import { Features } from './components/Features';
+import { Services } from './components/Services';
+import { Process } from './components/Process';
+import { Stats } from './components/Stats';
+import { Work } from './components/Work';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
+
+const App: React.FC = () => {
+  return (
+    <div className="relative min-h-screen w-full overflow-hidden bg-black text-white selection:bg-primary selection:text-black">
+      {/* Dynamic Liquid Background */}
+      <Background />
+
+      {/* Content Wrapper */}
+      <div className="relative z-10 flex flex-col w-full">
+        <Header />
+        
+        <main className="flex flex-col items-center w-full">
+          <div className="w-full max-w-[1400px] px-4 sm:px-6 md:px-8 lg:px-12 space-y-24 md:space-y-32 pb-24">
+            <Hero />
+            <Features />
+            <Services />
+            <Process />
+            <Stats />
+            <Work />
+            <Contact />
+          </div>
+        </main>
+
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default App;
